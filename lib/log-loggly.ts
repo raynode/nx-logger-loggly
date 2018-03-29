50325fe83d7d76d45ff8e2bf86e087e7753d161f
Tobias Kopelke <tobias.kopelke@liquidlabs.de>
2018-03-29 10:28:30 +0200

    fix(migration): update usage of nxLogger

--- lib/log-loggly.ts
+++ lib/log-loggly.ts
@@ -1,10 +1,10 @@
 
-import { formatMessage, nxLogger } from '@raynode/nx-logger'
+import { formatMessage, TransportFn } from '@raynode/nx-logger'
 import { createClient, Instance, Options, useInstanceOrCreateClient } from './loggly-client'
 
 export { Options, Instance }
 
-export const createTransport = (options: Options | Instance): nxLogger.TransportFn => {
+export const createTransport = (options: Options | Instance): TransportFn => {
   const client = useInstanceOrCreateClient(options)
   return (configuration, messages) =>
     client.log(formatMessage(messages), configuration.namespace)
